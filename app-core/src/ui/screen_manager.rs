b23cba641d224a8ef08e889d3e0869dc1b61d591
Naeem Razali <naeemrazali@yahoo.com>
2026-06-15 23:37:10 +1200

    remove deprecated functions and includes for actor model

--- app-core/src/ui/screen_manager.rs
+++ app-core/src/ui/screen_manager.rs
@@ -1,8 +1,4 @@
-use embedded_graphics::pixelcolor::Gray8;
-use embedded_graphics::prelude::DrawTarget;
-
-use crate::player::Player;
-use crate::ui::event::Event;
+use crate::event::Event;
 use crate::ui::screen_names::ScreenName;
 use crate::ui::screens::main_screen::MainScreen;
 use crate::ui::screens::settings_screen::SettingsScreen;
@@ -24,31 +20,14 @@ impl Default for ScreenManager {
 }
 
 impl ScreenManager {
-    pub fn handle_event(&mut self, event: &Event, player: &mut Player) {
+    pub fn handle_event(&mut self, event: &Event) {
         let next_screen = match self.current_screen {
-            ScreenName::Main => self.main.handle_event(event, player),
-            ScreenName::Settings => self.settings.handle_event(event, player),
+            ScreenName::Main => self.main.handle_event(event),
+            ScreenName::Settings => self.settings.handle_event(event),
         };
 
         if let Some(new_screen) = next_screen {
             self.current_screen = new_screen;
-        }
-    }
-
-    pub fn sync(&mut self, player: &Player) {
-        match self.current_screen {
-            ScreenName::Main => self.main.sync(player),
-            ScreenName::Settings => self.settings.sync(player),
-        }
-    }
-
-    pub fn draw(
-        &self,
-        display: &mut impl DrawTarget<Color = Gray8, Error = impl core::fmt::Debug>,
-    ) {
-        match self.current_screen {
-            ScreenName::Main => self.main.draw(display),
-            ScreenName::Settings => self.settings.draw(display),
         }
     }
 }
