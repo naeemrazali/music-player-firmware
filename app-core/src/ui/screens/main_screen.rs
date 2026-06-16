74fe18433765b93794d2b7d2e65b67b1b1b0bf1d
Naeem Razali <naeemrazali@yahoo.com>
2026-06-16 20:03:34 +1200

    refactor main screen to actor model

--- app-core/src/ui/screens/main_screen.rs
+++ app-core/src/ui/screens/main_screen.rs
@@ -2,70 +2,22 @@ use embedded_graphics::mono_font::{MonoTextStyle, ascii::FONT_6X10};
 use embedded_graphics::pixelcolor::Gray8;
 use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::Rectangle;
+use heapless::Vec;
 
-use crate::player::Player;
+use crate::event::{Button, Event, Playback, Screen};
+use crate::track::Track;
 use crate::ui;
-use crate::ui::event::{Button, Event};
 use crate::ui::screen_names::ScreenName;
 
 pub struct MainScreen {
-    pub background: Gray8,
-    pub title: ui::Label<'static>,
-    pub artist: ui::Label<'static>,
-    pub elapsed_time: ui::Label<'static>,
-    pub total_time: ui::Label<'static>,
-    pub progress: ui::ProgressBar,
-    pub play_button: ui::PlayButton,
-}
-
-impl MainScreen {
-    pub fn sync(&mut self, player: &Player) {
-        if let Some(track) = player.current_track() {
-            self.title.set_text(track.title);
-            self.artist.set_text(track.artist);
-        }
-        self.play_button.is_playing = player.is_playing();
-        self.progress.percent = player.progress_percent();
-        self.elapsed_time
-            .set_text(core::str::from_utf8(&ui::fmt_time_ms(player.elapsed_ms())).unwrap());
-        self.total_time
-            .set_text(core::str::from_utf8(&ui::fmt_time_ms(player.total_ms())).unwrap());
-    }
-
-    pub fn handle_event(&mut self, event: &Event, player: &mut Player) -> Option<ScreenName> {
-        match event {
-            Event::ButtonPress(Button::Play) => {
-                player.toggle_playback();
-                None
-            }
-            Event::ButtonPress(Button::Menu) => Some(ScreenName::Settings),
-            Event::ButtonPress(Button::Next) => {
-                player.next_track();
-                None
-            }
-            Event::ButtonPress(Button::Prev) => {
-                player.prev_track();
-                None
-            }
-            Event::Seek(ms) => {
-                player.seek_to(*ms);
-                None
-            }
-        }
-    }
-
-    pub fn draw(
-        &self,
-        display: &mut impl DrawTarget<Color = Gray8, Error = impl core::fmt::Debug>,
-    ) {
-        ui::clear_background(display, self.background);
-        self.title.draw(display);
-        self.artist.draw(display);
-        self.elapsed_time.draw(display);
-        self.total_time.draw(display);
-        self.progress.draw(display);
-        self.play_button.draw(display);
-    }
+    background: Gray8,
+    title: ui::Label<'static>,
+    artist: ui::Label<'static>,
+    elapsed_time: ui::Label<'static>,
+    total_time: ui::Label<'static>,
+    progress: ui::ProgressBar,
+    play_button: ui::PlayButton,
+    events: Vec<Event, 8>,
 }
 
 impl Default for MainScreen {
@@ -99,6 +51,93 @@ impl Default for MainScreen {
                 Gray8::BLACK,
             )),
             play_button: (ui::PlayButton::new(Point::new(120, 170), 12, Gray8::BLACK, false)),
+            events: Vec::new(),
         }
+    }
+}
+
+impl MainScreen {
+    pub fn handle_event(&mut self, event: &Event) {
+        match event {
+            Event::ButtonPress(Button::Play) => {
+                self.add_event(Event::Player(Playback::Toggle));
+            }
+            Event::ButtonPress(Button::Menu) => {
+                self.add_event(Event::Ui(Screen::Change(ScreenName::Settings)))
+            }
+            Event::ButtonPress(Button::Next) => {
+                self.add_event(Event::Player(Playback::NextTrack));
+            }
+            Event::ButtonPress(Button::Prev) => {
+                self.add_event(Event::Player(Playback::PreviousTrack));
+            }
+            Event::ButtonPress(Button::Seek(ms)) => {
+                self.add_event(Event::Player(Playback::Seek(*ms)));
+            }
+            Event::Player(Playback::TrackChanged(track)) => {
+                self.sync_new_track(track);
+                self.add_event(Event::Ui(Screen::Refresh));
+            }
+            Event::Player(Playback::Toggled(is_playing)) => {
+                self.update_play_button(*is_playing);
+                self.add_event(Event::Ui(Screen::Refresh));
+            }
+            Event::Player(Playback::ProgressUpdated {
+                elapsed_ms,
+                percent_elapsed,
+            }) => {
+                self.update_progress_bar(*elapsed_ms, *percent_elapsed);
+                self.add_event(Event::Ui(Screen::Refresh));
+            }
+            _ => (),
+        }
+    }
+
+    pub fn event_queue(&mut self) -> Vec<Event, 8> {
+        let mut queue = Vec::new();
+        core::mem::swap(&mut self.events, &mut queue);
+        queue
+    }
+
+    fn add_event(&mut self, event: Event) {
+        let _ = self.events.push(event);
+    }
+
+    fn draw(&self, display: &mut impl DrawTarget<Color = Gray8, Error = impl core::fmt::Debug>) {
+        ui::clear_background(display, self.background);
+        self.title.draw(display);
+        self.artist.draw(display);
+        self.elapsed_time.draw(display);
+        self.total_time.draw(display);
+        self.progress.draw(display);
+        self.play_button.draw(display);
+    }
+
+    pub fn sync_new_track(&mut self, track: &Option<Track>) {
+        match track {
+            Some(track) => {
+                self.title.set_text(track.title);
+                self.artist.set_text(track.artist);
+                self.elapsed_time.set_text("00:00");
+                self.total_time
+                    .set_text(core::str::from_utf8(&ui::fmt_time_ms(track.duration_ms)).unwrap());
+            }
+            None => {
+                self.title.set_text(Track::default().title);
+                self.artist.set_text(Track::default().artist);
+                self.elapsed_time.set_text("00:00");
+                self.total_time.set_text("00:00");
+            }
+        }
+    }
+
+    fn update_progress_bar(&mut self, elapsed_ms: u32, percent_elapsed: u32) {
+        self.progress.percent = percent_elapsed;
+        self.elapsed_time
+            .set_text(core::str::from_utf8(&ui::fmt_time_ms(elapsed_ms)).unwrap());
+    }
+
+    fn update_play_button(&mut self, is_playing: bool) {
+        self.play_button.is_playing = is_playing;
     }
 }
