72cd7a768837478559c18e33087d12b2d63494da
naeemrazali <naeemrazali@yahoo.com>
2026-09-18 21:46:31 +1200

    use heapless string for track struct

--- app-core/src/track.rs
+++ app-core/src/track.rs
@@ -1,18 +1,12 @@
-#[derive(Copy, Clone)]
-pub struct Track {
-    pub title: &'static str,
-    pub artist: &'static str,
-    pub duration_ms: u32,
-    pub file_path: &'static str,
-}
+use heapless::String;
 
-impl Default for Track {
-    fn default() -> Self {
-        Self {
-            title: "",
-            artist: "",
-            duration_ms: 0,
-            file_path: "",
-        }
-    }
+const MAX_PATH_LENGTH: usize = 128;
+const MAX_STR_LENGTH: usize = 64;
+
+#[derive(Clone, Default)]
+pub struct Track {
+    pub title: String<MAX_STR_LENGTH>,
+    pub artist: String<MAX_STR_LENGTH>,
+    pub duration_ms: u32,
+    pub file_path: String<MAX_PATH_LENGTH>,
 }
